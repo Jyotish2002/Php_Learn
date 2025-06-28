0a25f0f76f16763cfc60bcc151c505d8aa1571a2
Jyotish <jyotishyadav09@gmail.com>
2025-06-29 05:17:29 +0530

    Revise all function of array

---
+++ 13_Array-function/array.php
@@ -0,0 +1,28 @@
+<?php
+    $users=["Jyotish","Sam","Pratham","Tony","Sam","Pratham"];
+    // echo var_dump($users); we can check with this also
+    // echo is_array($users); //fn to check array
+    // echo "<br/>";
+    // var_dump(is_array($users));
+    // echo count($users);
+    // unset($users[2]); // unset woi element ko remove kr deta ha
+    // print_r($users);
+
+    // array_push($users,"Rohan");
+    // array_pop($users);
+    // print_r($users);
+    $user=["name"=>"Jyotish","age"=>22,"email"=>"jyotish@google.com"];
+    // print_r(array_keys($user));
+
+    // echo implode($user); //convert array to string
+    $str="Hello are you i am fine";
+    //--> convert string to array
+//    print_r(explode(" ",$str));
+
+// $data= (array_merge($user,$users)); //Merge two data
+// print_r($data);
+// When there is duplicate element in an array then-->
+    $data=array_unique($users);
+    print_r($data);
+
+?>
