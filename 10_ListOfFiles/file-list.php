c3118d8ba802bdbe2ab30108721396bf37fca942
Jyotish <jyotishyadav09@gmail.com>
2025-06-29 04:36:43 +0530

    file-list

---
+++ 10_ListOfFiles/file-list.php
@@ -0,0 +1,11 @@
+<?php
+    $path="files";
+    $items=scandir($path);
+    
+    $items = array_diff($items,[".",".."]);
+    foreach($items as $item){
+        echo "<a href=./files/$item>$item</a>";
+        echo "<br/>";
+    }
+
+?>
