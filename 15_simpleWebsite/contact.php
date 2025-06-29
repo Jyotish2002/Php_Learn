04a52770a0a7e746670082399e85a5be9a55a566
Jyotish <jyotishyadav09@gmail.com>
2025-06-29 23:48:48 +0530

    Basic webpage

---
+++ 15_simpleWebsite/contact.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Contact Page</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <div class="container">
+        <?php
+        include_once("header.php");
+        ?>
+        <h1 class="heading">Contact Page</h1>
+        <p>Id ad ea esse deserunt esse tempor veniam laborum. Proident proident voluptate esse mollit aliquip nostrud sit aliqua. Lorem do aute ut esse. Quis qui pariatur eu cupidatat aute amet ea id ex. Adipisicing enim sunt ex occaecat incididunt pariatur eu non amet.</p>
+
+        <p>Dolore dolor quis minim minim. Voluptate duis aliquip deserunt proident dolor aliqua Lorem labore enim. Irure incididunt ex reprehenderit ad anim dolore aliquip amet.</p>
+
+        <p>Laborum est cillum do enim culpa. Amet aliquip velit est sunt laborum cillum adipisicing. Ullamco dolore non Lorem deserunt exercitation voluptate occaecat aliqua.</p>
+
+        <p>Tempor nisi ipsum aliquip amet ipsum nostrud excepteur ea. In cillum dolor ipsum adipisicing minim dolore enim in duis. Quis amet enim ex duis amet proident officia anim aliquip.</p>
+
+        <p>Sint velit est quis minim excepteur esse. Occaecat est nisi minim exercitation ad proident esse Lorem sit. Esse veniam reprehenderit sunt veniam Lorem duis reprehenderit. Reprehenderit non adipisicing laboris nulla laboris sint deserunt quis eiusmod. Eiusmod quis aliquip nostrud id.</p>
+        <?php
+        include_once("footer.php");
+        ?>
+    </div>
+</body>
+
+</html>
